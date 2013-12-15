70e02eaebf05ffd1ccdcce84f7fbe03071dc7a50
Alexander Kaydalov <akaydalov@gilt.com>
2013-12-15 22:21:00 +0200

    hist added

---
+++ bash.h
@@ -0,0 +1,500 @@
+cd ..
+ls
+cd data/
+ls
+cd foresee/
+ls
+cd outgoing/
+ls
+cd current/
+ls
+ls
+cd /
+ls
+cd /web
+ls
+cd dw_in
+cd dw_dint/
+ls
+cd bin/
+ls
+cd ..
+ls
+cd target/
+ls
+cd scala-2.9.1/
+ls
+cd ..
+ls
+cd ..
+ls
+cd bin
+ls
+cat dint_outgoing.sh 
+ls
+pwd
+cd ..
+ls
+pwd
+find / log4j.properties
+find . log4j.properties
+find . |grep log4
+cd /web
+find . |grep log4
+pwd
+cd dw_dint/
+ls
+cd /web
+ls
+find . |grep .log
+find . |grep Outgoing.log
+find . |grep fesee
+find . 
+find . |grep log
+ssh etl4
+find . 
+find . run.rb
+find . run.rb
+man find
+man find
+cd /web
+find . -name run.rb
+find . -name *.log
+pwd
+cd /web/var/log/
+ls
+cat dw-agent.log 
+ls
+pwd
+cd /web/dw_dint/config/
+ls
+cd /web/var/log
+ls
+ls
+pwd
+cat /web/dw_dint/config/log4j.properties
+ды
+ls
+cat Foresee.log 
+cat Foresee.log 
+ssh etl4
+cat /web/var/log/Foresee.log 
+ssh etl4
+ls
+ftp clientsftp3.foreseeresults.com
+ls
+pwd
+cd /web/var/log
+ls
+cat Foresee.log 
+cat /web/var/data/foresee/outgoing/current/foresee_fulfillment_2013-12-05.csv
+ls /web/var/data/foresee/outgoing/current/
+ls
+cd /web/var/data/foresee/outgoing/current/
+ls
+tail -f /web/var/log/Foresee.log 
+cd /web/dw-agent/
+git status
+git pull --rebase origin master
+git status
+git pull --rebase origin master
+git pull --rebase origin akaydalov
+ssh etl4
+pwd
+git status
+git rebase --continue
+git add sql/dml/user_summary/user_summary/user_summary.sql
+git rebase --continue
+git status
+git pull --rebase origin akaydalov
+git status
+git pull --rebase origin akaydalov
+git pull --rebase origin akaydalov
+mkdir /web2
+sudo mkdir /web2
+ls
+cd /web2
+ls
+ls
+ls -la
+sudo chown . akaydalov
+sudo chown akaydalov /web2
+ls -la
+pwd
+git clone ssh://gerrit.gilt.com:29418/dw-agent.git
+ls
+cd dw-agent/
+ls
+cd sql/
+ls
+cd dml/
+ls
+cd user_summary/
+ls
+cd ..
+ls
+pwd
+cd /web2
+git merge origin/akaydalov
+git fetch
+pwd
+cd dw-agent/
+LS
+ls
+git merge origin/akaydalov
+ls
+cd sql
+ls
+cd dml
+ls
+cd user_summary/
+ls
+cd user_summary/
+ls
+cat user_summary.sql 
+git branch -r akaydalov
+git branch -r
+git checkout akaydalov
+ls
+cat user_summary.sql 
+pwd
+git status
+ls
+cat user_summary.sql 
+cd /web
+ls
+cd dw-agent/
+ls
+git status
+git fetch
+ssh etl4
+cd /web/dw-agent/
+cat README.md 
+ssh etl4
+ls
+pwd
+cd ..
+sl
+ls
+cd dw-agent/
+ls
+cat README.md 
+ls
+cat README.md 
+ssh etl4
+ssh etl1
+ssh etl4
+cd /web/dw-agent/
+ls
+cd ../dw_dint/
+ls
+ls
+cat README.md 
+ls
+ls
+pwd
+cat README.md 
+sbt eclipse
+cd /web
+ls
+cd var
+ls
+cd data/
+ls
+cd foresee/
+ls
+cd outgoing/
+ls
+cd current/
+ls
+ls -la
+cd /web
+ls
+cd dw-schema/
+ls
+cat README.md 
+jpwf
+f
+pwd
+git fetch
+ls
+pwd
+ls
+cp ~/Downloads/update_script_pmuntyanu.sql /web/dw-schema/
+cd /web/dw-schema/
+ls
+cat README.md 
+bin/add.rb ./update_script_pmuntyanu.sql 
+gem install rdoc
+ls
+pwd
+cd /web
+ls
+cd var/data/
+ls
+cd foresee
+ls
+cd outgoing/
+ls
+cd ..
+ls
+cd ..
+ls
+cd foresee_answered/
+ls
+cd outgoing/current/
+ls
+car foresee_answered_V1_sample_2013-12-11.csv
+cat foresee_answered_V1_sample_2013-12-11.csv
+ls /System/Library/Frameworks/Ruby.framework/Versions/Current
+ls /System/Library/Frameworks/Ruby.framework/Versions/Current
+ls -la gl-ml-akaydalov:current akaydalov$ ls /System/Library/Frameworks/Ruby.framework/Versions/Current
+cd /web/var/
+ls
+cd data/
+ls
+cd foresee_answered/
+LS
+ls
+cd outgoing/
+ls
+cd current/
+ls
+ls -la
+cat foresee_answered_V1_sample_2013-12-11.csv 
+зцв
+pwd
+cd /web/dw-agent/
+ls
+git log
+ls
+ls
+ssh etl4
+ssh etl1
+ssh etl1
+ssh etl1
+ping etl1
+ssh etl1
+ssh etl4
+sudo sudo shutdown -r now
+cd /web/dw-agent/
+ls
+git status
+git pull
+cd /web
+git clone ssh://akaydalov@gerrit.gilt.com:29418/dw_etl
+pwd
+cd dw_etl/
+ls
+brew install ack
+ack hopper
+ack hopper ./*
+ack user ./*
+ssh etl1
+ls
+cd /web/dw-sch
+cd /web/dw-schema/
+ls
+cd script
+ls
+cd sql
+ls
+pwd
+ls
+cd src/
+cd ..
+ls
+cd ..
+ls
+cd script
+ls
+cd ../scripts
+ls
+cat * |grp marked_paid_in_full_at_ny
+cat * |grep marked_paid_in_full_at_ny
+cat scripts.iml 
+rm -r scripts.iml src
+ls
+cat * |grep marked_paid_in_full_at_ny
+cd ..
+ls
+cd ..
+ls
+pwd
+cd dw-schema/
+ls
+git pull
+git fetch
+ls
+cd script
+pwd
+cd ..
+ls
+cd ..
+ls
+rm -r dw-schema
+ls
+rm -r dw-schema
+pwd
+cd dw-agent/
+ls
+cd config/
+ls
+cat jobs.yml |grep unit_event_stream_insert_receipt
+ssh etl1
+ssh etl1
+cd /web/dw-agent/
+ls
+cd sql/dml/user_summary/
+ls
+cd user_summary/
+ls
+exit
+/Volumes/My\ Passport/Project/Yotta/out ; exit;
+ls
+git status
+git fetch
+git status
+ls
+ls
+cd /web/var/log/dw-agent.log 
+cd /web/var/
+ls
+cd data
+ls
+cd foresee
+ls
+ls -la
+cd outgoing/
+ls
+cd current/
+ls -la
+cat foresee_fulfillment_V2_sample_2013-12-12.csv
+ping nclusterq1.prod.iad.gilt.local
+ssh etl1
+cd
+cd Downloads/
+ls
+ls gilt*
+cd /web
+ls
+cd var
+ls
+cd data
+ls
+cd foresee
+ls
+cd outgoing/
+ls
+cd current/
+ls
+ls -la
+gzip foresee_fulfillment_V2_sample_2013-12-12.csv
+ls -la
+jls
+ls
+ls
+sftp clientsftp3.foreseeresults.com
+sftp ftp.gilt@clientsftp3.foreseeresults.com
+cd /web2
+ls
+rm -r dw-agent/
+pwd
+rm -r dw-agent/
+y
+y
+y
+ls
+ls
+git clone https://github.com/akaydalov/testrepo.git
+ls
+ls
+cd testrepo/
+ls
+git status
+git log > lot.one
+git log
+ls
+git status
+git add -a 
+git add lot.one 
+git status
+git commit -a
+git status
+git push origin master
+git push origin master
+git status
+git reset HEAD~1
+git status
+git log
+git reflog
+git branch -r
+git branch akaidalov
+git status
+git branch -r
+git checkout akaidalov
+git status
+ls
+vi lot.one
+git checkout master
+vi lot.one
+git checkout akaidalov
+git status
+git add lot.one 
+git status
+git checkout master
+ls
+cat lot.one 
+git status
+git reset HEAD lot.one 
+cat lot.one 
+git checkout akaidalov
+git status
+git add lot.one 
+git status
+git commit 
+git status
+git checkout master
+cat lot.one 
+git checkout akaidalov
+cat lot.one 
+git push origin
+git status
+git push origin akaidalov
+git branch -r
+git branch
+git status
+git fetch
+git status
+ls
+cat lot.one 
+git pull
+git pull origin akaidalov
+cat lot.one 
+git status
+git checkout master
+ls
+git branch -r
+cat lot.one 
+vi lot.one 
+git status
+git add lot.one 
+git status
+git commit -a
+git status
+git checkout akaidalov
+cat lot.one 
+git merge master
+cat lot.one 
+git status
+git checkout master
+git reset HEAD
+cat lot.one 
+git reset
+git reset hard origin/akaidalov
+git reset HEAD
+git reset --hard  HEAD
+cat lot
+cat lot.one 
+git status
+git log --oneline --graph --decorate --all
